4219a02a4a9075730a8a158603315057f44fc324
bbjbc <jobc4550@gmail.com>
2024-02-23 20:01:23 +0900

    feat: 유효성 검증 추가

--- src/components/form/add-form.tsx
+++ src/components/form/add-form.tsx
@@ -15,6 +15,12 @@ function AddForm({ onSubmit, placeholder }: Props) {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
+
+    if (addDetail.trim() === '') {
+      console.error('입력값이 없습니다. 입력해주세요.')
+      return
+    }
+
     onSubmit(addDetail)
     setAddDetail('')
   }
@@ -26,6 +32,7 @@ function AddForm({ onSubmit, placeholder }: Props) {
         value={addDetail}
         onChange={(e) => setAddDetail(e.target.value)}
         placeholder={placeholder}
+        required
       />
       <Button type="submit">추가</Button>
     </form>
