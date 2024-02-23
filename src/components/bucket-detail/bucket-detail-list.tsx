7b377db982b8daa8bee64ca748094134e3c10346
bbjbc <jobc4550@gmail.com>
2024-02-23 20:04:03 +0900

    refactor: bucket detail list 양식 수정

--- src/components/bucket-detail/bucket-detail-list.tsx
+++ src/components/bucket-detail/bucket-detail-list.tsx
@@ -1,44 +1,126 @@
-import React from 'react'
+'use client'
 
-import { Checkbox } from '../ui/checkbox'
-import { Label } from '../ui/label'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { toast } from '@/components/ui/use-toast'
 
 interface BucketDetailListProps {
+  slug: string
   dummyData: string[]
   checkedItems: Set<number>
   handleCheckboxChange: (index: number) => void
 }
 
-function BucketDetailList({
+const FormSchema = z.object({
+  dummyData: z.array(z.string()).refine((value) => value.length > 0, {
+    message: '체크가 되지 않았습니다. 체크 해주세요!',
+  }),
+})
+
+export default function BucketDetailList({
+  slug,
   dummyData,
   checkedItems,
   handleCheckboxChange,
 }: BucketDetailListProps) {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      dummyData: [],
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: '당신이 제출한 데이터입니다.',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+
   return (
-    <div className="mx-12 overflow-y-auto">
-      <div className="mx-5">
-        {dummyData.map((data, index) => (
-          <div key={index} className="m-5">
-            <div className="space-x-6">
-              <Checkbox
-                id={`checkbox-${index}`}
-                checked={checkedItems.has(index)}
-                onCheckedChange={() => handleCheckboxChange(index)}
-              />
-              <Label
-                htmlFor={`checkbox-${index}`}
-                className={
-                  checkedItems.has(index) ? 'text-xl line-through' : 'text-xl'
-                }
-              >
-                {data}
-              </Label>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="relative mx-10 my-4 h-screen space-y-8"
+      >
+        <FormField
+          control={form.control}
+          name="dummyData"
+          render={() => (
+            <FormItem>
+              <div className="my-2 flex flex-col space-y-4">
+                <div className="w-full">
+                  <FormLabel className="mx-2 text-2xl font-bold">
+                    세부 목표
+                  </FormLabel>
+                  <hr className="mx-2 my-2" />
+                  <FormDescription className="text-md mx-4">
+                    {`${slug}에 대한 세부 목표입니다.`}
+                  </FormDescription>
+                </div>
+                <div className="max-h-96 w-full overflow-y-auto">
+                  {dummyData.map((item, index) => (
+                    <FormItem
+                      key={index}
+                      className="mx-6 my-3 items-start space-x-4 space-y-0"
+                    >
+                      <FormControl>
+                        <Checkbox
+                          name={`dummyData.${index}`}
+                          id={`dummyData.${index}`}
+                          checked={
+                            form.getValues('dummyData').includes(item) &&
+                            checkedItems.has(index)
+                          }
+                          onCheckedChange={(checked) => {
+                            handleCheckboxChange(index)
+                            const currentItems = form.getValues('dummyData')
+                            const updatedItems = checked
+                              ? [...currentItems, item]
+                              : currentItems.filter((value) => value !== item)
+                            form.setValue('dummyData', updatedItems)
+                          }}
+                        />
+                      </FormControl>
+                      <FormLabel
+                        htmlFor={`dummyData.${index}`}
+                        className={
+                          checkedItems.has(index)
+                            ? 'text-xl line-through'
+                            : 'text-xl'
+                        }
+                      >
+                        {item}
+                      </FormLabel>
+                    </FormItem>
+                  ))}
+                </div>
+              </div>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="absolute bottom-0 w-full">
+          달성 완료
+        </Button>
+      </form>
+    </Form>
   )
 }
-
-export default BucketDetailList
