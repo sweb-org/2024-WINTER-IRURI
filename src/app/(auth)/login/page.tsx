d0b72764d3c5770c1846ea4f75bd39a62d3ea752
조성원 <tjddnjsjo@kakao.com>
2024-03-04 18:04:06 +0900

    feat: 로그인 페이지 회원가입 네비게이션 추가

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,13 +1,22 @@
+import Link from 'next/link'
+
+import { cn } from '@/lib/utils'
 import { LoginForm } from '@/components/login/login-form'
-import { Button } from '@/components/ui/button'
+import { buttonVariants } from '@/components/ui/button'
 
 export default function Login() {
   return (
     <main className="container">
       <LoginForm />
-      <Button variant="ghost" size="lg" className="w-full">
+      <Link
+        href="/signup"
+        className={cn(
+          buttonVariants({ variant: 'outline', size: 'lg' }),
+          'w-full',
+        )}
+      >
         회원가입
-      </Button>
+      </Link>
     </main>
   )
 }
